294e0cb2058c2eca3e3e92aed159d53a7dd72618
vask19 <88391656+vask19@users.noreply.github.com>
2021-11-03 13:25:58 +0300

    6

--- File Server/task/src/main/Data.java
+++ File Server/task/src/main/Data.java
@@ -5,5 +5,5 @@ public class Data {
     public static final int SERVER_PORT = 9000;
     public static final String SERVER_DATA_PATH = "C:\\Users\\vask\\IdeaProjects\\File Server\\File Server\\task\\src\\server\\data\\";
     public static final String CLIENT_DATA_PATH = "C:\\Users\\vask\\IdeaProjects\\File Server\\File Server\\task\\src\\client\\data\\";
-
+    public static final String FILE_WITH_ID_AND_NAMES = "C:\\Users\\vask\\IdeaProjects\\File Server\\File Server\\task\\src\\main\\nameAndId.bin";
 }
--- File Server/task/src/main/MapHandler.java
+++ File Server/task/src/main/MapHandler.java
@@ -1,25 +1,79 @@
 package main;
 
-import java.io.File;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 import java.util.UUID;
 
-public class MapHandler {
-    private static volatile Map<String, UUID> filesNamesAndId;
+public class MapHandler implements AutoCloseable{
+    private static volatile Map<String, String > filesNamesAndId;
+    private static ObjectOutputStream outputStream;
+    private static ObjectInputStream inputStream;
 
 
 
 
-
-    private void readMap(){
+    public static void main(String[] args) {
         filesNamesAndId = new HashMap<>();
+        filesNamesAndId.put("1","2");
+        writeToFileWithDataFiles();
+        System.out.println(readFileWithDataFiles());
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    private static void writeToFileWithDataFiles(){
+        try(ObjectOutputStream outputStream = new ObjectOutputStream(
+                new FileOutputStream(Data.FILE_WITH_ID_AND_NAMES)
+        )){
+            outputStream.writeObject(filesNamesAndId);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
 
 
 
+    private static Map<String,String> readFileWithDataFiles(){
+
+        HashMap map = new HashMap<>();
+        try(ObjectInputStream inputStream = new ObjectInputStream(
+                new FileInputStream(Data.FILE_WITH_ID_AND_NAMES))) {
+            map  = (HashMap) inputStream.readObject();
+
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+        /*
+
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(new FileInputStream(Data.FILE_WITH_ID_AND_NAMES)))){
+            String line;
+            while ((line = reader.readLine()) != null){
+                String fileName = line.split(" ")[0];
+                String fileId = line.split(" ")[1];
+                map.put(fileName,fileId);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return map;*/
+        return map;
+    }
     private  String createUniqueNameToFile(String ext){
         Random random = new Random();
         File dir = new File(Data.SERVER_DATA_PATH);
@@ -39,5 +93,8 @@ public class MapHandler {
 
 
 
-
+    @Override
+    public void close() throws Exception {
+        writeToFileWithDataFiles();
+    }
 }
